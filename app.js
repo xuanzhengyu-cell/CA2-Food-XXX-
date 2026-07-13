2896dc9133a02b9344ef57cf2888aecaa20dc033
chanyanLah <25027490@myrp.edu.sg>
2026-07-13 11:23:26 +0800

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ connection.connect((err) => {
     console.log('Connected to MySQL database'); 
 }); 
  
-// Set up view engine hi
+// Set up view engine 
 app.set('view engine', 'ejs'); 
 //  enable static files 
 app.use(express.static('public')); 
