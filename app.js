712403a769c843b9557f79702cc09d2559688507
Xuan Zhengyu <xuanzhengyu0827@gmail.com>
2026-07-17 07:46:38 +0000

    Registration, Login, Access Control

--- app.js
+++ app.js
@@ -30,7 +30,10 @@
     // 
     //    - Non-authorised users can only read; they cannot do anything else. 
 
-//    - Regardless, remember to ensure that the SQL table (for data) can accept it.   
+//    - Regardless, remember to ensure that the SQL table (for data) can accept it.  
+// Note that the encryption of the password is SHA2 (?, 256)
+
+
 // Import required modules
 const express = require('express'); 
 const mysql = require('mysql2'); 
@@ -55,15 +58,26 @@ const connection = mysql.createConnection({
     host: 'localhost', 
     user: 'root', 
     password: 'RP738964$', 
-    database: 'c237_010_team5_ca2_db' 
+    database: 'c237_010_team5_ca2_db',
+    ssl: {
+         rejectUnauthorized: false
+    }   
 }); 
+connection.connect((err) => { 
+    if (err) { 
+        console.error('Error connecting to MySQL:', err); 
+        return; 
+    } 
+    console.log('Connected to MySQL database'); 
+}); 
+
 /* 
 side note:
 - Table 1: users
   - user_id
   - username
   - password
-  - role
+  - role (admin / group_owner / group_member / normal_user)
   - location_id (Can be left empty)
 
 - Table 2: location
@@ -85,21 +99,14 @@ side note:
   - comments_text
   - date 
 */
-connection.connect((err) => { 
-    if (err) { 
-        console.error('Error connecting to MySQL:', err); 
-        return; 
-    } 
-    console.log('Connected to MySQL database'); 
-}); 
 
 // Session Creation
 app.use(session({
     secret: 'secret',
     resave: false,
     saveUninitialized: true,
-    // Session expires after 1 week of inactivity
-    cookie: {maxAge: 1000 * 60 * 60 * 24 * 7}
+    // Session expires after 1 day of inactivity
+    cookie: {maxAge: 1000 * 60 * 60 * 24 }
 }));
 
 // Set up view engine 
@@ -109,15 +116,130 @@ app.use(express.static('public'));
 app.use(express.urlencoded ({
   extended: false
 }));
+ 
+app.use(flash());
+
 // Set Up login, registration, access control 
-// Route for login
+    // For access control (Check Admin, Check Group Owner + Admin, Check Group Owner + Member + admin)
+        // Middleware to check if user is logged in
+const checkAuthenticated = (req, res, next) => {
+    if (req.session.user) {
+        return next();
+    } else {
+        req.flash('error', 'Please log in to use this resource');
+        res.redirect('/login');
+    }
+};
+
+        // Admin Check (For the Admin's Dashboard entry)
+const checkAdmin = (req, res, next) => {
+    if (req.session.user.role === 'admin') {
+    return next();
+    } else {
+    req.flash('error', 'Access denied');
+    res.redirect('/dashboard');
+    }
+};
+
+        // Check Group Owner + Admin (For the location group mod dashboard)
+const checkGOwnerandAdmin = (req, res, next) => {
+    const id = req.params.id
+    if (req.session.user.role === 'admin') {
+        return next();
+    }
+    if (req.session.user.role === "group_owner" && String(req.session.user.location_id) === String(id)) {
+        return next();
+    }
+    req.flash('error', 'Access denied');
+    res.redirect('/dashboard');
+};
+
+        // Check Group Owner + Member + admin (For sending messages in the location groups)
+const checkGOwnerandAdmin = (req, res, next) => {
+    const id = req.params.id
+    if (req.session.user.role === 'admin') {
+        return next();
+    }
+    if (req.session.user.role === "group_owner" && String(req.session.user.location_id) === String(id)) {
+        return next();
+    }
+    if (req.session.user.role === "group_member" && String(req.session.user.location_id) === String(id)) {
+        return next();
+    }
+    req.flash('error', 'Access denied');
+    res.redirect('/dashboard');
+};
+
+    // Route for login
+app.get('/login', (req, res) => {
+    res.render('login', { messages: req.flash('success'), errors: req.flash('error') });
+});
+    // Process login
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    // Validate email and password
+    if (!username || !password) {
+        req.flash('error', 'All fields are required.');
+        return res.redirect('/login');
+    }
+
+    const sql = 'SELECT * FROM users WHERE username = ? AND password = SHA2(?, 256)';
+    connection.query(sql, [username, password], (err, results) => {
+        if (err) {
+            throw err;
+        }
+        // Save the credentials into a session cookie
+        if (results.length > 0) {
+            // Successful login
+            req.session.user = results[0]; 
+            req.flash('success', 'Login successful!');
+            res.redirect('/');
+        
+        } else {
+            // Invalid credentials
+            req.flash('error', 'Invalid email or password.');
+            res.redirect('/login');
+        }
+    });
+});
 
 
-// Route for registration
+    // Route for registration
+app.get('/register', (req, res) => {
+    res.render('register', { messages: req.flash('error'), formData: req.flash('formData')[0] });
+});
 
+        // validate that the items entered are viable
+const validateRegistration = (req, res, next) => {
+    const { username, password, re_password } = req.body;
+    if (!username || !password || !re_password) {
+        return res.send('All fields are required.');
+    }
+    if (password.length < 6) {
+        req.flash('error', 'Password should be at least 6 or more characters long');
+        req.flash('formData', req.body);
+        return res.redirect('/register');
+    }
+    if (password != re_password)
+        return res.send('Password are not the same.');
+next();
+};
 
-// For access control
-
+    // Process registration
+app. post('/register',validateRegistration, (req, res) => {
+    const {username, password} = req.body;
+    const role = "normal_user"
+    const sql = 'INSERT INTO users (username, password, role) VALUES (?, SHA2(?, 256), ?)';
+    db.query(sql, [username, password, role], (err, result) => {
+        if (err) {
+            throw err;
+        }
+        console.log(result);
+        req.flash('success', 'Registration successful! Please log in.');
+        res.redirect('/login');
+    });
+});
 
 // Route: Edit of <>
 // Show the search page with all stalls by default
