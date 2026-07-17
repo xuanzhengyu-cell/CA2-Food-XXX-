4b56adff7ad81ab86740627af8fa6cb715700a31
Xuan Zhengyu <xuanzhengyu0827@gmail.com>
2026-07-17 06:12:58 +0000

    DB details

--- app.js
+++ app.js
@@ -1,63 +1,9 @@
-// Import required modules
-const express = require('express'); 
-const mysql = require('mysql2'); 
-const session = require('express-session');
-const flash = require('connect-flash');
-const app = express(); 
-const multer = require ('multer');
-
-// Set up multer for file uploads
-const storage = multer.diskStorage({
-destination: (req, file, cb) => {
-cb(null, 'public/images'); 
-},
-filename: (req, file, cb) => {
-cb(null, file.originalname); 
-}
-});
-const upload = multer({ storage: storage });
- 
-// Create MySQL connection 
-const connection = mysql.createConnection({ 
-    host: 'localhost', 
-    user: 'root', 
-    password: 'RP738964$', 
-    database: 'to_be_confirmed' 
-}); 
-connection.connect((err) => { 
-    if (err) { 
-        console.error('Error connecting to MySQL:', err); 
-        return; 
-    } 
-    console.log('Connected to MySQL database'); 
-}); 
-
-// Session Creation
-app.use(session({
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: true,
-    // Session expires after 1 week of inactivity
-    cookie: {maxAge: 1000 * 60 * 60 * 24 * 7}
-}));
-
-// 
-
-
-// Set up view engine 
-app.set('view engine', 'ejs'); 
-//  enable static files 
-app.use(express.static('public')); 
-app.use(express.urlencoded ({
-  extended: false
-}));
-
 // Note: Our Theme will be on Food Store
 
 // Assets
-// - 4 sql tables: one for username + password +  user id + position + location id (if any) 
+// - 4 sql tables: one for username + password +  user id + roles + location id (if any) 
 //                 one for location id, location name
-//                 one for messages id, location id, store id, text, date, favourites
+//                 one for messages id, location id, owner_id, sender_id, text, date, likes
 //                 one for comments id, user id,  comments text
 // - 4 (? may add more) positions. 
 // - Projected pages: Login page, Home page (*display all groups existing + admin messages), Group page (to display data), Adding page, Editing page, 
@@ -84,12 +30,98 @@ app.use(express.urlencoded ({
     // 
     //    - Non-authorised users can only read; they cannot do anything else. 
 
-//    - Regardless, remember to ensure that the SQL table (for data) can accept it. 
+//    - Regardless, remember to ensure that the SQL table (for data) can accept it.   
+// Import required modules
+const express = require('express'); 
+const mysql = require('mysql2'); 
+const session = require('express-session');
+const flash = require('connect-flash');
+const app = express(); 
+const multer = require ('multer');
+
+// Set up multer for file uploads
+const storage = multer.diskStorage({
+destination: (req, file, cb) => {
+cb(null, 'public/images'); 
+},
+filename: (req, file, cb) => {
+cb(null, file.originalname); 
+}
+});
+const upload = multer({ storage: storage });
+ 
+// Create MySQL connection 
+const connection = mysql.createConnection({ 
+    host: 'localhost', 
+    user: 'root', 
+    password: 'RP738964$', 
+    database: 'c237_010_team5_ca2_db' 
+}); 
+/* 
+side note:
+- Table 1: users
+  - user_id
+  - username
+  - password
+  - role
+  - location_id (Can be left empty)
+
+- Table 2: location
+  - location_id
+  - location_name
+
+- Table 3: messages
+  - messages_id
+  - location_id (The page that has the message)
+  - sender_id (user_id of the user who sent the message)
+  - message_text
+  - date
+  - likes
+
+- Table 4: comments 
+  - comments_id
+  - sender_id
+  - owner_id (The user whose page has this comment)
+  - comments_text
+  - date 
+*/
+connection.connect((err) => { 
+    if (err) { 
+        console.error('Error connecting to MySQL:', err); 
+        return; 
+    } 
+    console.log('Connected to MySQL database'); 
+}); 
+
+// Session Creation
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    // Session expires after 1 week of inactivity
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 7}
+}));
+
+// Set up view engine 
+app.set('view engine', 'ejs'); 
+//  enable static files 
+app.use(express.static('public')); 
+app.use(express.urlencoded ({
+  extended: false
+}));
+// Set Up login, registration, access control 
+// Route for login
+
+
+// Route for registration
+
+
+// For access control
 
 
 // Route: Edit of <>
 // Show the search page with all stalls by default
-app.get('/', (req, res) => {
+app. get('/', (req, res) => {
   connection.query('SELECT * FROM stores', (err, results) => {
     if (err) throw err;
     res.render('results', { stalls: results, filters: {} });
@@ -97,7 +129,7 @@ app.get('/', (req, res) => {
 });
 
 // Handle search + filters
-app.get('/stalls', (req, res) => {
+app.post('/stalls', (req, res) => {
   const { search, location, cuisine, rating, price } = req.query;
 
   // Start with a base query, add conditions only for filters that were used
